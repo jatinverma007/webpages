ed20f304721ec0e35daaa66c8e5a26f5b2799ccc
jatin verma <jatin.v1997@gmail.com>
2015-07-18 19:44:05 +0530

    a webpage made by html css

---
+++ 7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
+
+</head>
+
+<body>
+<div class="nav">
+    <div class="container">
+    <img src=""
+    </div>
+</div>
+
